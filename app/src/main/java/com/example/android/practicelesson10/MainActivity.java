b8858e6495033a2b0c13e1bc2e1c85432ef9c851
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-02 10:57:28 -0500

    Romove onCreate method.

--- app/src/main/java/com/example/android/practicelesson10/MainActivity.java
+++ app/src/main/java/com/example/android/practicelesson10/MainActivity.java
@@ -7,18 +7,5 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        TextView seeText = new TextView(this);
-        seeText.setAllCaps(true);
-        seeText.setText("I'm calling methods on this object!");
-        seeText.setTextColor(Color.CYAN);
-        seeText.setTextSize(60);
-        seeText.setPadding(16,16,16,16);
-        seeText.setShadowLayer(5,3,3,Color.DKGRAY);
-
-        setContentView(seeText);
-    }
 }
