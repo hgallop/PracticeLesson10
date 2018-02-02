4140241a0970642b308f8c658d7c5e921003066c
Heather Gallop <doyouseeitmyway@gmail.com>
2018-02-02 09:22:52 -0500

    Create object with constructor. Call methods on object. Add object to setContentView.

--- app/src/main/java/com/example/android/practicelesson10/MainActivity.java
+++ app/src/main/java/com/example/android/practicelesson10/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.android.practicelesson10;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView seeText = new TextView(this);
+        seeText.setAllCaps(true);
+        seeText.setText("I'm calling methods on this object!");
+        seeText.setTextColor(Color.CYAN);
+        seeText.setTextSize(60);
+        seeText.setPadding(16,16,16,16);
+        seeText.setShadowLayer(5,3,3,Color.DKGRAY);
+
+        setContentView(seeText);
     }
 }
